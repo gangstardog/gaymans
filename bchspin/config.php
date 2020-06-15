c21580eb3f70bb547b221d34f972d67b16c3d977
gangstardog <57928162+gangstardog@users.noreply.github.com>
2020-06-15 20:45:36 -0300

    Add files via upload

---
+++ bchspin/config.php
@@ -0,0 +1,6 @@
+<?php
+
+$wallet_bch = "qzxlyvyajm38mldlld3axnfvnunpr8pmuurlpltkfl";
+$target_wd = 0.00005000;
+
+$key[] = "4407b489472ca437b3838d1399ac84d4148cb349101e408e14fdf5e6f0fa5c2fff41845737afb182a12675a4076c30106f2d";
